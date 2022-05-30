38a95381a695baac1170c91084cd495bb7553b92
robpduo <101683611+robpduo@users.noreply.github.com>
2022-05-30 19:18:35 -0400

    add some button functionality:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var tasksToDoE1  = document.querySelector("#tasks-to-do");
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() {
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
-  }
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-buttonEl.addEventListener("click", createTaskHandler);
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
